12fc2406fa058f096d5563b2ccec857000b079dd
ash <as_supp@icloud.com>
2024-09-25 16:59:11 +0200

    bug fixes

--- check-pwa.js
+++ check-pwa.js
@@ -1,10 +1,9 @@
 // Check if the app is running in standalone mode or on iOS/macOS
 const displayMode = window.matchMedia('(display-mode: standalone)').matches;
 const isStandalone = window.navigator.standalone;
-const body = document.body;
-const bfLogo = document.querySelector('.bfLogo');
 
 if (displayMode || isStandalone) {
-  body.classList.add('pwa');
-  bfLogo.classList.add('pwa');
+  document.querySelectorAll('*').forEach(element => {
+    element.classList.add('pwa');
+  });  
 }
